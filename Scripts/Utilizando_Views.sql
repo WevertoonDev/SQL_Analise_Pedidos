04c856a83cacc5f0e983656586cb56d34d5dbfe9
Weverton Martins <wevertonm22@icloud.com>
2026-05-03 00:46:15 -0300

    Utilizando_Views

---
+++ Scripts/Utilizando_Views.sql
@@ -0,0 +1,16 @@
+SELECT * from ViewValorTotalPedido;
+
+SELECT nome from ViewValorTotalPedido;
+
+SELECT * from ViewValorTotalPedido
+WHERE ValorTotalPedido = 10
+
+SELECT * from ViewValorTotalPedido
+WHERE ValorTotalPedido > 10
+
+SELECT * from ViewValorTotalPedido
+WHERE ValorTotalPedido > 10 and ValorTotalPedido < 14;clientes
+
+SELECT * from ViewValorTotalPedido
+WHERE strftime('%m', datahorapedido) = '08'
+
