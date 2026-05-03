f18aa8a8e5b8870fb27af3927c1578132bbcb78c
Weverton Martins <wevertonm22@icloud.com>
2026-05-03 00:28:51 -0300

    Criando_Views

---
+++ Scripts/Criando_Views.sql
@@ -0,0 +1,30 @@
+SELECT * FROM pedidos
+SELECT * FROM itenspedidos
+SELECT * from produtos
+
+SELECT p.id, c.nome, p.datahorapedido, SUM(ip.precounitario) AS ValorTotalPedido
+FROM clientes c 
+join pedidos p on c.id = p.idcliente
+join itenspedidos ip ON p.id = ip.idpedido
+GROUP BY p.id, c.nome
+
+SELECT c.nome AS NomeCliente, SUM(ip.precounitario) AS SomaTotalPedidos
+from clientes c 
+INNER join pedidos p 
+on c.ID = p.idcliente
+INNER JOIN itenspedidos ip
+on ip.idpedido
+GROUP by c.nome
+
+CREATE VIEW ViewCliente AS 
+SELECT nome,endereco FROM clientes;
+
+SELECT * FROM ViewCliente
+
+CREATE view ViewValorTotalPedido AS
+SELECT p.id, c.nome, p.datahorapedido, SUM(ip.precounitario) AS ValorTotalPedido
+FROM clientes c 
+join pedidos p on c.id = p.idcliente
+join itenspedidos ip ON p.id = ip.idpedido
+GROUP BY p.id, c.nome;
+SELECT * from ViewValorTotalPedido;
