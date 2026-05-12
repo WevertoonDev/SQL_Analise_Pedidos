9ad8a6de33f268fb1f6e92d93a2f5d5fd797cbf4
Weverton Martins <wevertonm22@icloud.com>
2026-05-11 21:56:15 -0300

    removedo teste

--- onteste.sql
+++
@@ -1,7 +0,0 @@
-SELECT * FROM colaboradores  WHERE id = 3
-DELETE from colaboradores WHERE id = 3
-
-SELECT * from clientes WHERE id = 27
-SELECT * from pedidos WHERE idcliente = 27
-DELETE from clientes WHERE id = 27
-on DELETE CASCADE
